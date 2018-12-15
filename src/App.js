4a3ebd6fda7f30a62e54562f421c90c6c329c850
s5802496 <joseph.deluca@scotiabank.com>
2018-12-14 19:02:49 -0500

    React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
 import './App.css';
 
@@ -7,7 +8,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Main />
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }

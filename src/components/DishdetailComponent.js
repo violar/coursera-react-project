ee0d493d5c641bfea187b00e11455b13b5246b38
s5802496 <joseph.deluca@scotiabank.com>
2018-12-16 22:49:20 -0500

    react assignment 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -56,7 +56,6 @@ import { Link } from 'react-router-dom';
                 </div>
                 <div className="row">
                     <div className="col-12 col-md-5 m-1">
-                        
                         <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes'; 
 import { COMMENTS } from '../shared/comments';
@@ -49,6 +50,7 @@ class Main extends Component {
         <Header />
         <Switch>
             <Route path="/home" component={HomePage} />
+            <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
             <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
             <Route path='/menu/:dishId' component={DishWithId} />
             <Route exact path="/contactus" component={Contact} />

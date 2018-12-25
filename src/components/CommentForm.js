93c1c079850343b8df2c8808dc76ca23ef4ae6d4
s5802496 <joseph.deluca@scotiabank.com>
2018-12-25 15:23:19 -0500

    Combining Reducers

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,8 +1,6 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Row, Label, Col } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import { Button, Modal, ModalBody, ModalHeader, Label, Row, Col } from 'reactstrap';
 import { Control, LocalForm, Errors} from 'react-redux-form';
-
 
 const minLength = (len) => (val) => val && val.length >= len;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -95,69 +93,5 @@ class CommentForm extends Component{
     }
 }
 
-    function RenderDish({dish}){
-        return (
-            <React.Fragment>
-                <Card>
-                    <CardImg top width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            </React.Fragment>
-        );
-    }
+export default CommentForm;
 
-    function RenderComments({comments}){
-        if (comments == null) {
-            return (<div></div>);
-        }
-
-        return ( 
-            <React.Fragment>
-                <h4>Comments</h4>
-                <ul className="list-unstyled">    
-                    {comments.map(c => {
-                        return <li key={c.id}>
-                                    <p>{c.comment}</p>
-                                    <p>-- {c.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(c.date)))}</p>
-                                </li>
-                    })}
-                </ul>
-                <CommentForm />
-            </React.Fragment>
-        ); 
-    }
-
-    function DishDetail(props){
-        if (props.dish == null) {
-            return (<div></div>);
-            
-        }
-
-        return (  
-            <div className="container">
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>{props.dish.name}</h3>
-                        <hr />
-                    </div>                
-                </div>
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish} />
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} />
-                    </div>
-                </div>
-                </div>
-        )
-    }
-
-export default DishDetail;
